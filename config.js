7581c06629a5914d644601c881c98f7ea925f209
choomuangchoosak4289 <choomuangchoosak4289@gmail.com>
2026-08-29 15:10:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+window.SAIS_CONFIG = {
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycby-SWiAlG9YWR05CbpaaslGUEml893AeQKGMzQEbia3B8db4Bf-loaHIT3fVori8XQ/exec",
+    ADMIN_USERNAME: "jirapong"
+};
