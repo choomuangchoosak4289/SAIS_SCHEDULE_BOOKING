9440dff6ed2a3d87652854e1c99c58d687c1b9ea
choomuangchoosak4289 <choomuangchoosak4289@gmail.com>
2026-08-30 16:50:57 +0700

    Update SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 window.SAIS_CONFIG = {
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxUKeUB_QMdkcisPWkQN-ysEg2ggYloaGQQaByu6MXeyJk-PHCXgUa9qe_ae2-ttkos/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxk74JQ9MLxUAmkXr8WNgrDXoFhDq90kNolKBV6oUnCQ9J252J5VNwfZkCXzDX934F9/exec",
     ADMIN_USERNAME: "jirapong",
     API_KEY: "SAIS_SECURE_KEY_2026"
 };
