7f49ca96073b6545a43f74db4b6716ed958b3f42
choomuangchoosak4289 <choomuangchoosak4289@gmail.com>
2026-08-30 16:24:32 +0700

    Add configuration settings in config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+window.SAIS_CONFIG = {
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxFy3XoXPZACB90DoC3mcF0yRO28uYArMYcakcl_CwsC86xPxFhd0vp6uyYAxv4E-zaTw/exec",
+    ADMIN_USERNAME: "jirapong",
+    API_KEY: "SAIS_SECURE_KEY_2026"
+};
